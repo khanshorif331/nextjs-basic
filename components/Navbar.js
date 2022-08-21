b83dd66bfae8ec57f4021135f7fe29226aab645e
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 01:46:56 +0600

    link compoonent added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,20 @@
+import Link from 'next/link'
+
 const Navbar = () => {
 	return (
 		<nav>
 			<div className='logo'>
 				<h1>Shorif List</h1>
 			</div>
-			<a>Home</a>
-			<a>About</a>
-			<a>Shorif Listing</a>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='/about'>
+				<a>About</a>
+			</Link>
+			<Link href='/shorif'>
+				<a>Shorif Listing</a>
+			</Link>
 		</nav>
 	)
 }
