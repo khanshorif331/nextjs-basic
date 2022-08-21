c3f2fd5afa0e4cff758c41d8383259c599abba6f
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 01:28:34 +0600

    naavabar updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,11 @@ const Navbar = () => {
 	return (
 		<nav>
 			<div className='logo'>
-				<h1>Ninja List</h1>
+				<h1>Shorif List</h1>
 			</div>
+			<a>Home</a>
+			<a>About</a>
+			<a>Shorif Listing Listing</a>
 		</nav>
 	)
 }
