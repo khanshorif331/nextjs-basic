7b7eeb73f79fc9a4e7bc5159f41d27f41bca3275
khanshorif331 <khanshorif331@gmail.com>
2022-08-21 22:53:42 +0600

    index js page updated

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,16 @@ export default function Home() {
 	return (
 		<div>
 			<h1>This is homepage</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+				maxime, quia cumque veniam consequatur quod nam minus itaque
+				suscipit iste.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+				maxime, quia cumque veniam consequatur quod nam minus itaque
+				suscipit iste.
+			</p>
 		</div>
 	)
 }
