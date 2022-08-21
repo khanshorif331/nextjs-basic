597f3af5537c3af2ef2baf82de4ed230fa950c4d
khanshorif331 <khanshorif331@gmail.com>
2022-08-21 22:52:25 +0600

    about page created

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+				maxime, quia cumque veniam consequatur quod nam minus itaque
+				suscipit iste.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+				maxime, quia cumque veniam consequatur quod nam minus itaque
+				suscipit iste.
+			</p>
+		</div>
+	)
+}
+
+export default About
