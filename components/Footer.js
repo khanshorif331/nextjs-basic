96dc686947951b00c8b4b792bf02c4c4362f47ce
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 02:21:38 +0600

    updated thecode

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<div>
+		<footer>
 			<p>Copyright 2022 All Rights Reserved</p>
 			<p>
 				Made with <span style={{ color: 'red' }}>&hearts;</span> by Shoriful
 				Islam
 			</p>
-		</div>
+		</footer>
 	)
 }
 
