1cb93d832dbca4958aa753968c336af71ba3edf1
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 01:40:16 +0600

    heart looogoo added in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<div>
-			<p>Copyright 2022 Shorif List</p>
+			<p>Copyright 2022 All Rights Reserved</p>
+			<p>
+				Made with <span style={{ color: 'red' }}>&hearts;</span> by Shoriful
+				Islam
+			</p>
 		</div>
 	)
 }
