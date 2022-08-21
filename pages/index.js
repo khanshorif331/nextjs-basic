b0c85eed3dfeaca581b6534c4993696eaef0b025
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 01:56:12 +0600

    updaated the routing system

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -23,6 +24,9 @@ export default function Home() {
 				elit. Aut maxime, quia cumque veniam consequatur quod nam minus
 				itaque suscipit iste.
 			</p>
+			<Link href='/shorif'>
+				<a>See Shorif Listing</a>
+			</Link>
 			<Footer></Footer>
 		</div>
 	)
