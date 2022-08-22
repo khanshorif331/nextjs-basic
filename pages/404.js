312c7148625d3213a3f398cb7d9b52eec4d86bb4
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 20:21:07 +0600

    user redirecting to the homepage from notfound

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,14 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+	const router = useRouter()
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/')
+		}, 3000)
+	}, [])
 	return (
 		<div className='not-found'>
 			<h1>Oooooops...!</h1>
