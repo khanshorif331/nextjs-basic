f390a3484a46b90b7248de55b9f30aba3f626647
khanshorif331 <khanshorif331@gmail.com>
2022-08-22 19:47:34 +0600

    added some more info in the homepaage

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,43 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 	return (
 		<div>
-			{/* <Navbar></Navbar> */}
 			<h1>This is homepage</h1>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
 				maxime, quia cumque veniam consequatur quod nam minus itaque
 				suscipit iste. Lorem ipsum dolor sit amet, consectetur adipisicing
 				elit. Aut maxime, quia cumque veniam consequatur quod nam minus
-				itaque suscipit iste.
+				itaque suscipit iste.Lorem ipsum dolor sit amet, consectetur
+				adipisicing elit. Aut maxime, quia cumque veniam consequatur quod
+				nam minus itaque suscipit iste. Lorem ipsum dolor sit amet,
+				consectetur adipisicing elit. Aut maxime, quia cumque veniam
+				consequatur quod nam minus itaque suscipit iste.
 			</p>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
 				maxime, quia cumque veniam consequatur quod nam minus itaque
 				suscipit iste. Lorem ipsum dolor sit amet, consectetur adipisicing
 				elit. Aut maxime, quia cumque veniam consequatur quod nam minus
-				itaque suscipit iste.
+				itaque suscipit iste.Lorem ipsum dolor sit amet, consectetur
+				adipisicing elit. Aut maxime, quia cumque veniam consequatur quod
+				nam minus itaque suscipit iste. Lorem ipsum dolor sit amet,
+				consectetur adipisicing elit. Aut maxime, quia cumque veniam
+				consequatur quod nam minus itaque suscipit iste.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+				maxime, quia cumque veniam consequatur quod nam minus itaque
+				suscipit iste. Lorem ipsum dolor sit amet, consectetur adipisicing
+				elit. Aut maxime, quia cumque veniam consequatur quod nam minus
+				itaque suscipit iste.Lorem ipsum dolor sit amet, consectetur
+				adipisicing elit. Aut maxime, quia cumque veniam consequatur quod
+				nam minus itaque suscipit iste. Lorem ipsum dolor sit amet,
+				consectetur adipisicing elit. Aut maxime, quia cumque veniam
+				consequatur quod nam minus itaque suscipit iste.
 			</p>
 			<Link href='/shorif'>
 				<a>See Shorif Listing</a>
 			</Link>
-			{/* <Footer></Footer> */}
 		</div>
 	)
 }
