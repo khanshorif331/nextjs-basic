e51f52bd744290065699e540278623ab2ef96a7d
khanshorif331 <khanshorif331@gmail.com>
2022-08-24 01:30:29 +0600

    lesson 12 done

--- pages/shorif/[id].js
+++ pages/shorif/[id].js
@@ -1,3 +1,21 @@
+export const getStaticPaths = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+	const data = await res.json()
+
+	const paths = data.map(sho => {
+		return {
+			params: {
+				id: sho.id.toString(),
+			},
+		}
+	})
+
+	return {
+		paths,
+		fallback: false,
+	}
+}
+
 const Details = () => {
 	return (
 		<div>
