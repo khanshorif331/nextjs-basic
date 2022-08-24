5792f93658e7136da636b740ac09b8847fd25409
khanshorif331 <khanshorif331@gmail.com>
2022-08-25 01:16:28 +0600

    single data is fetching by id

--- pages/shorif/[id].js
+++ pages/shorif/[id].js
@@ -1,5 +1,5 @@
 export const getStaticPaths = async () => {
-	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
 	const data = await res.json()
 
 	const paths = data.map(sho => {
@@ -16,10 +16,26 @@ export const getStaticPaths = async () => {
 	}
 }
 
-const Details = () => {
+export const getStaticProps = async context => {
+	const id = context.params.id
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+	const data = await res.json()
+	return {
+		props: {
+			shorif: data,
+		},
+	}
+}
+
+const Details = ({ shorif }) => {
 	return (
 		<div>
-			<h1>Details Page</h1>
+			<h1>{shorif.name}</h1>
+			<p>{shorif.email}</p>
+			<p>{shorif.website}</p>
+			<p>{shorif.address.city}</p>
+			<p>{shorif.email}</p>
+			<p>{shorif.email}</p>
 		</div>
 	)
 }
